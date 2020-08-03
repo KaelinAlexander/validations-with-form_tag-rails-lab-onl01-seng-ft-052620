17795b554ab24af440aea8bfccac62407870b536
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-08-03 16:33:31 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,13 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+
+  end
+
   def edit
     @post = Post.find(params[:id])
   end
