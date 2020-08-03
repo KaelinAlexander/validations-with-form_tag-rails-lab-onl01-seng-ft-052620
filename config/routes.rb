ce4690e1a662567dd6257b529ef0f01e0574b5a9
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-08-03 16:46:21 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :authors, only: [:new, :edit, :show]
+  resources :authors, only: [:show, :new, :create, :edit, :update]
   patch 'authors/:id', to: 'authors#update'
+  
   resources :posts, only: [:new, :edit, :show]
 end
